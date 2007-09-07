c4764102436325f9899540b3a7a234dc3cb6182e
adamrbrown <adamrbrown@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-09-07 18:04:40 +0000

    v1.03
    git-svn-id: https://plugins.svn.wordpress.org/kb-linker/trunk@18181 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- README.txt
+++ README.txt
@@ -3,7 +3,7 @@ Contributors: adamrbrown
 Donate link: http://adambrown.info/b/widgets/donate/
 Tags: link, seo, links
 Requires at least: 2.0
-Tested up to: 2.2.1
+Tested up to: 2.2.2
 Stable tag: trunk
 
 Looks for user-defined phrases in posts and automatically links them to specified URLs. Example: Link every occurrence of "Wordpress" to wp.org.
@@ -14,7 +14,7 @@ KB Linker will link phrases you specify to sites you specify. For example, you c
 
 You'll find more detailed instructions on the admin page. But really, it's pretty easy. And KB Linker does the hard work of (a) making sure that words aren't already linked and (b) making sure only whole words, not partial words, get linked.
 
-The plugin is commented extensively, so if you want to tweak its behavior, give it a go.
+The plugin file is commented extensively, so if you want to tweak its behavior, give it a go.
 
 = Support =
 
@@ -30,7 +30,9 @@ All done. Go to your blog's home page and admire all the automatically inserted 
 
 == License ==
 
-This plugin is provided "as is" and without any warranty or expectation of function. I'll probably try to help you if you ask nicely, but I can't promise anything. You are welcome to use this plugin and modify it however you want, as long as you give credit where it is due.
+This plugin is provided "as is" and without any warranty or expectation of function. I'll probably try to help you if you ask nicely, but I can't promise anything. You are welcome to use this plugin and modify it however you want, as long as you give credit where it is due. 
+
+But please don't redistribute this plugin from anywhere other than right here. Unless months go by and it looks like I've completely abandoned this thing, let's not get forks going without a darn good reason; that just confuses people. But send me your improvements and I'll add them in and include a shout-out to you here.
 
 == Screenshots ==
 
@@ -44,9 +46,17 @@ Well, that's obviously up to you. Maybe you don't. But if there is something tha
 
 This plugin could also serve SEO (search engine optimization) purposes if you're into that sort of thing; you could use it to ensure plenty of internal cross-linking between your posts.
 
-= I want the plugin to behave slightly differently. =
+= Can I make the links open in new windows? =
 
-Take a look at the plugin file. I marked up the code heavily to make it easy to tweak. After looking at the code, if you have a question, you can post a comment on the plugin's page.
+Yes, as of version 1.03. See the instructions on the plugin's admin page.
+
+= I have problems on my non-English blog. =
+
+Tweak the setting at the top of the plugin file.
+
+= Does the plugin do X? =
+
+Probably not. But this is a simple piece of code, so try modifying it to suit your whims. Take a look at the plugin file. I marked up the code heavily to make it easy to tweak. After looking at the code, if you have a question, you can post a comment on the plugin's page.
 
 = I have a question that isn't addressed here. =
 
--- kb_linker.php
+++ kb_linker.php
@@ -4,33 +4,50 @@ Plugin Name: KB Linker
 Plugin URI: http://adambrown.info/b/widgets/kb-linker/
 Description: Looks for user-defined phrases in posts and automatically links them. Example: Link every occurrence of "Wordpress" to wordpress.org.
 Author: Adam R. Brown
-Version: 1.02
+Version: 1.03
 Author URI: http://adambrown.info/
 */
+
+//	OPTIONAL SETTINGS
+	/*special characters for foreign languages. Add any you want to the array below. Char goes on left, HTML entity on right. The German codes are here as examples.
+		See http://www.w3schools.com/tags/ref_entities.asp for HTML entities.	*/
+	$kblinker_special_chars = array(
+				'ä' => '&#228;',
+				'Ä' => '&#196;',
+				'ö' => '&#246;',
+				'Ö' => '&#214;',
+				'ü' => '&#252;',
+				'Ü' => '&#220;',
+				'ß' => '&#223;'
+	);
+//	END OF SETTINGS. NO MORE EDITING REQUIRED.
+
 
 /*	 CHANGE LOG
 	1.01	initial upload
 	1.02	quick fix to check is_array before extracting on line 105
-*/
+	1.03	- added support for German (or other) special characters
+		- added support for opening links in different targets
 
-/* 
+
+ 
 	IMPORTANT NOTE TO ANYBODY CONSIDERING ADDING THIS PLUGIN TO A WP-MU INSTALLATION:
 	If you aren't sure whether you are using a WP-MU blog, then you aren't. Trust me. If this warning applies to you, then you will know it.
 	For WP-MU administrators: You should not use this plugin. Your users could use it to place (potentially malicious) javascript into their blogs.
 	This plugin is PERFECTLY SAFE for non-WP-MU blogs, so ignore this message if you're using regular wordpress (you probably are).
-*/
 
-/*		DEVELOPMENT NOTES. If you're trying to modify something, read the following comments.
-		GENERAL NOTE:
-			You will notice that I've included extensive commenting in the code below. I do not have time to support this plugin. The commenting is there to make this plugin
-			easy to modify. Please try making modifications on your own before posting a support question at the plugin's URI.
-			That being said, you are welcome to post well-informed support questions on my site.
-		DATABASE STRUCTURE
-			the options->KB Linker page will create a set of matching terms and URLs that gets stored as a list.
-			structure of option "kb_linker":
-				pairs => array( see below)
-				text=>	same content as pairs, but in unprocessed form (for displaying in the option's form)
-				plurals => 1, 0		if 1, we should look for variants of the keywords ending in s or es
+
+	FOR DEVELOPERS, HACKERS, MODDERS, WHATEVER. If you're trying to modify something, read the following comments.
+	GENERAL NOTE:
+		You will notice that I've included extensive commenting in the code below. I do not have time to support this plugin. The commenting is there to make this plugin
+		easy to modify. Please try making modifications on your own before posting a support question at the plugin's URI.
+		That being said, you are welcome to post well-informed support questions on my site.
+	DATABASE STRUCTURE
+		the options->KB Linker page will create a set of matching terms and URLs that gets stored as a list.
+		structure of option "kb_linker":
+			pairs => array( see below)
+			text=>	same content as pairs, but in unprocessed form (for displaying in the option's form)
+			plurals => 1, 0		if 1, we should look for variants of the keywords ending in s or es
 */
 
 function kb_linker($content){
@@ -41,23 +58,47 @@ function kb_linker($content){
 	if ( !is_array($pairs) )
 		return $content;
 
-	if ( 1 != $plurals ){	 // we do basically the same thing whether we're looking for plurals or not. The non-plurals case is easier, so here it is first:
-		foreach ($pairs as $keyword => $url){
-			// we don't want to link the keyword if it occurs within another link.
-			// so let's find all instances where the keyword is in a link and replace it with something innocuous. Let's use &&&, since WP would pass that
-			// to us as &amp;&amp;&amp; (if it occured in a post), so it would never be in the $content on its own.
-			// this has two steps. First, look for the keyword as linked text:
-			$content = preg_replace( '|(<a[^>]+>)(.*)('.$keyword.')(.*)(</a.*>)|U', '$1$2&&&$4$5', $content);
-			// Second, look for the keyword inside tags. E.g. if they're linking every occurrence of "Google" manually, we don't want to find 
-			// <a href="http://google.com"> and change it to <a href="http://<a href="http://www.google.com">.com">
-			// More broadly, we don't want them linking anything that might be in a tag. (e.g. linking "strong" would screw up <strong>). Here goes:
-			$content = preg_replace( '|(<[^>]*)('.$keyword.')(.*>)|U', '$1&&&$3', $content);
-			// I'm sure a true master of regular expressions wouldn't need the previous two steps, and would simply write the following expression better. But this works.
+	// let's make use of that special chars setting.
+	global $kblinker_special_chars;
+	if (is_array($kblinker_special_chars)){
+		foreach ($kblinker_special_chars as $char => $code){
+			$content = str_replace($code,$char,$content);
+		}
+	}
 
-			// now that we've taken the keyword out of any links it appears in, let's look for the keyword elsewhere.
-			$content = preg_replace( '|(?<=[\s>;"\'/])('.$keyword.')(?=[\s<&.,!\'";:\-/])|i', '<a href="'.$url.'" class="kblinker">$1</a>', $content, 1);	// that "1" at the end limits it to replacing the keyword only once per post.
-			
-			/* some notes about that regular expression:
+	// most of the action is in here.
+	foreach ($pairs as $keyword => $url){
+		// first, let's check whether we've got a "target" attribute specified. Let's not waste CPU resources unless we see a '[' in the URL:
+		if (false!==strpos( $url, ' ' ) ){
+			$target = trim(   substr( $url, strpos($url,' ') )   );
+			$target = 'target="'.$target.'"';
+			$url = substr( $url, 0, strpos($url,' ') );
+		}else{
+			$target='';
+		}
+		
+		// let's escape any '&' in the URL.
+		$url = str_replace( '&amp;', '&', $url ); // this might seem unnecessary, but it prevents the next line from double-escaping the &
+		$url = str_replace( '&', '&amp;', $url );
+		
+		// we don't want to link the keyword if it is already linked.
+		// so let's find all instances where the keyword is in a link and replace it with something innocuous. Let's use &&&, since WP would pass that
+		// to us as &amp;&amp;&amp; (if it occured in a post), so it would never be in the $content on its own.
+		// this has two steps. First, look for the keyword as linked text:
+		$content = preg_replace( '|(<a[^>]+>)(.*)('.$keyword.')(.*)(</a.*>)|U', '$1$2&&&$4$5', $content);
+
+		// Next, look for the keyword inside tags. E.g. if they're linking every occurrence of "Google" manually, we don't want to find 
+		// <a href="http://google.com"> and change it to <a href="http://<a href="http://www.google.com">.com">
+		// More broadly, we don't want them linking anything that might be in a tag. (e.g. linking "strong" would screw up <strong>). 
+		// if you get problems with KB linker creating links where it shouldn't, this is the regex you should tinker with, most likely. Here goes:
+		$content = preg_replace( '|(<[^>]*)('.$keyword.')(.*>)|U', '$1&&&$3', $content);
+
+		// I'm sure a true master of regular expressions wouldn't need the previous two steps, and would simply write the replacement expression (below) better. But this works for me.
+	
+		// now that we've taken the keyword out of any links it appears in, let's look for the keyword elsewhere.
+		if ( 1 != $plurals ){	 // we do basically the same thing whether we're looking for plurals or not. Let's do non-plurals option first:
+			$content = preg_replace( '|(?<=[\s>;"\'/])('.$keyword.')(?=[\s<&.,!\'";:\-/])|i', '<a href="'.$url.'" class="kblinker" '.$target.'>$1</a>', $content, 1);	// that "1" at the end limits it to replacing the keyword only once per post.
+			/* some notes about that regular expression to make modifying it easier for you if you're new to these things:
 			(?<=[\s>;"\'])
 				(?<=	marks it as a lookbehind assertion
 				to ensure that we are linking only complete words, we want keyword preceded by one of space, tag (>), entity (;) or certain kinds of punctuation (escaped with \ when necessary)
@@ -66,24 +107,13 @@ function kb_linker($content){
 				again, we link only complete words. Must be followed by space, tag (<), entity (&), or certain kinds of punctuation. 
 				Note that some of the punctuations are escaped with \
 			*/
-			
-			// restore the keyword when it occurs in links and tags:
-			$content = str_replace( '&&&', $keyword, $content);
+		}else{	// if they want us to look for plurals too:
+			// this regex is almost identical to the non-plurals one, we just add an s? where necessary:
+			$content = preg_replace( '|(?<=[\s>;"\'/])('.$keyword.'s?)(?=[\s<&.,!\'";:\-/])|i', '<a href="'.$url.'" class="kblinker" '.$target.'>$1</a>', $content, 1);	// that "1" at the end limits it to replacing once per post.
 		}
-	}else{	// if they want us to look for plurals too:
-		foreach ($pairs as $keyword => $url){
-			// look for the keyword in existing links -- remove it. Exact same as in non-plural case.
-			$content = preg_replace( '|(<a[^>]+>)(.*)('.$keyword.')(.*)(</a.*>)|U', '$1$2&&&$4$5', $content);
-			
-			// look for the keyword in tags -- remove it. Also same as non-plural case.
-			$content = preg_replace( '|(<[^>]*)('.$keyword.')(.*>)|U', '$1&&&$3', $content);
 
-			// now that we've taken the keyword out of any links it appears in, let's look for the keyword elsewhere.
-			$content = preg_replace( '|(?<=[\s>;"\'/])('.$keyword.'s?)(?=[\s<&.,!\'";:\-/])|i', '<a href="'.$url.'" class="kblinker">$1</a>', $content, 1);	// that "1" at the end limits it to replacing once per post.
-			
-			// restore the keyword when it occurs in links:
-			$content = str_replace( '&&&', $keyword, $content);
-		}
+		// restore the keyword when it occurs in links:
+		$content = str_replace( '&&&', $keyword, $content);
 	}
 	return $content;
 }
@@ -91,13 +121,15 @@ function kb_linker($content){
 
 function kb_linker_options_page(){
 	$sample = 'wordpress->http://wordpress.org/
-google->http://www.google.com/
+google->http://www.google.com/ _blank
+kb linker->http://adambrown.info/b/widgets/kb-linker/
 knuckleheads->http://www.house.gov/';
 
 	if ( $_POST['kb_linker'] ){
 		$pairs = str_replace("\r", '', $_POST['kb_linker']);
 		$pairs = explode("\n", $pairs);
 		foreach( $pairs as $pair ){
+			$pair = trim( $pair ); // no leading or trailing spaces. Can mess with the "target" thing in function kb_linker()
 			$pair = explode( "->", $pair );
 			if ( ( '' != $pair[0] ) && ( '' != $pair[1] ) )
 				$new[ $pair[0] ] = $pair[1];
@@ -125,7 +157,7 @@ knuckleheads->http://www.house.gov/';
 	<div class="wrap">
 	<h2>KB Linker</h2>
 	<p>KB Linker will link phrases you specify to sites you specify. For example, you could make it so that whenever "Wordpress" occurs in a post it is automatically linked to wordpress.org.</p>
-	<p>Enter your keyword-URL pairs in the box below. Each pair should appear on its own line. Separate each keyword from its respective link with "->". Look at the bottom of this page for important details. Here are a few examples to get you going:</p>
+	<p>Enter your keyword-URL pairs in the box below. Each pair should appear on its own line. Separate each keyword from its respective link with "->". Look at the bottom of this page for important details. Below are a few examples to get you going. Note that the link to Google will open in a new window, since it is followed with "&nbsp;_blank" (note the space).</p>
 	<blockquote><pre>'.$sample.'</pre></blockquote>
 	<p>Alright, knock yourself out:</p>
 	
@@ -144,6 +176,7 @@ knuckleheads->http://www.house.gov/';
 		<li>Keywords are case-insensitive (e.g. "wordpress" is the same as "WoRdPrEsS").</li>
 		<li>Spaces count, so "wordpress" is not the same as "wordpress ".</li>
 		<li>Keywords will be linked only if they occur in your post as a word (or phrase), not as a partial word. So if one of your keywords is "a" (for some strange reason), it will be linked only when it occurs as the word "a"--when the letter "a" occurs within a word, it will not be linked.</li>
+		<li>You can use any valid target attribute, not just "_blank"--see <a href="http://www.w3schools.com/tags/tag_a.asp">W3C</a> for a list of valid targets.</li>
 	</ul>
 	</div>
 	';
